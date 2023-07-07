8e5eccb14993a8746d6bdfb27dafc74d5bb5c02f
ronnioliveira <ronnioliveira@hotmail.com>
2023-07-07 17:09:16 -0300

    Creating app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <View>
+      <Text>TESTE TESTE</Text>
+    </View>
+  )
+}
+
+export default App
